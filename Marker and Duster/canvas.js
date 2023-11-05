9209a9b7cdba3be9627316702afc32ba489f76ee
devyanipathak <114909518+devyanipathak@users.noreply.github.com>
2023-11-05 07:16:12 +0530

    Files are added via upload.

---
+++ Marker and Duster/canvas.js
@@ -0,0 +1,9 @@
+let canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height= window.innerHeight;
+
+//API
+let tool = canvas.getContext("2d");
+tool.strokeStyle = "red";
+tool.lineWidth="3";
+
